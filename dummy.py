e924c0ffd7f988625e7a08b1105addcf99a6168e
rozyeastaugh <65141620+rozyeastaugh@users.noreply.github.com>
2023-03-07 15:59:56 -0800

    Add files via upload

--- dummy.py
+++ dummy.py
@@ -6,43 +6,76 @@ import pandas as pd
 import numpy as np
 import math
 import time
-
-
-def get_data():
-    # TODO: edit this filename if needed/add other files
-    raw_url = 'https://raw.githubusercontent.com/rozyeastaugh/cs238_finalproject/main/cs238_finalproject_sarsdata.csv'
-    download = requests.get(raw_url).content
-    df = pd.read_csv(io.StringIO(download.decode('utf-8')))
-    return df
-
-
-def get_states_actions(data):
-    S = np.array(data['s'])
-    # S = np.arange(1, 312020)
-    A = np.array(data['a'])
-    return S, A
+import matplotlib.pylab as plt
+import seaborn as sns
+from scipy.stats import multivariate_normal
 
 
 # Sets all policies to 0 except for 5 random positions on the grid, which is sets to 1 (add park)
 def get_policy(S, A, park_limit):
     S.sort()
     final_policy = {key: 0 for key in S}
-    parks = random.sample(list(S), park_limit)
+    # parks = random.sample(list(S), park_limit)
+    parks = [0, 1, 2, 3, 4]
     for p in parks:
         final_policy[p] = 1
     return final_policy
 
 
-def write_policy(p_dict, filename):
+def write_policy(final_policy, final_score, filename):
     with open(filename, 'w') as f:
-        for a in p_dict.values():
+        for a in final_policy.values():
             f.write("{}\n".format(a))
+        f.write("Final Score: {}\n".format(final_score))
+
+
+def simulate(prob_dist, S, R):
+    # TODO: pick 4 parks, set prob_dist to 0 for all of them
+    for s in range(len(S)):
+        if random.random() < prob_dist[s]:
+            # S[s][1] = 1
+            R[s] = -10  # If flooded, reward negatively TODO: try diff values here
+        else:
+            # S[s][1] = 0
+            R[s] = 10  # # If not flooded, reward poistively TODO: try diff values here
+    score = sum(R)
+    return score, R, S
+
+
+def gaussian_probs(num_states):
+    mean = [5, 5]
+    cov = [[5, 0], [0, 5]]
+    dist = multivariate_normal(mean, cov)
+    weights = []
+    for i in range(10):  # Calculate pdf for all grid points
+        for j in range(10):
+            weights.append(dist.pdf([i, j])/.03)  # TODO: added .03 so that prob in center was = .9, almost always floods
+    # weights = weights/sum(weights)
+    return weights
+
+
+def get_probabilities(num_states):
+    return gaussian_probs(num_states)  # TODO: try uniform and random as well for final paper
+
 
 
 park_limit = 5
-outputfilename = 'random.policy'
 start_time = time.time()
-data = get_data()
-S, A = get_states_actions(data)
+num_states = 100
+S = np.arange(0, num_states)
+A = np.arange(0, num_states)
 final_policy = get_policy(S, A, park_limit)
-write_policy(final_policy, outputfilename)
+
+# TO COMPARE TO FWD SEARCH
+R = np.zeros(num_states)
+prob_dist = get_probabilities(num_states)
+parks_indices = np.where(np.array(list(final_policy.values())) == 1)
+for p in list(parks_indices[0]):
+    prob_dist[p] = 0
+final_score, R, S = simulate(prob_dist, S, R)
+R_map = [R[0:10], R[10:20], R[20:30], R[30:40], R[40:50], R[50:60], R[60:70], R[70:80], R[80:90], R[90:100]]
+ax = sns.heatmap(R_map, linewidth=0.5)
+plt.title('Random Policy: Final Score = ' + str(final_score))
+
+plt.savefig('dummy')
+write_policy(final_policy, final_score, 'dummy.policy')
--- fwd_search.py
+++ fwd_search.py
@@ -7,12 +7,15 @@ import math
 import numpy as np
 import time
 from scipy.stats import multivariate_normal
+import matplotlib.pylab as plt
+import seaborn as sns
 
 
-def write_policy(policy, filename):
+def write_policy(final_policy, final_score, filename):
     with open(filename, 'w') as f:
-        for a in policy:  # TODO: check if policy is list, change if not
+        for a in final_policy:  # TODO: check if policy is list, change if not
             f.write("{}\n".format(a))
+        f.write("Final Score: {}\n".format(final_score))
 
 
 def random_probs(num_states):
@@ -33,68 +36,54 @@ def gaussian_probs(num_states):
     weights = []
     for i in range(10):  # Calculate pdf for all grid points
         for j in range(10):
-            weights.append(dist.pdf([i, j]))
-    probs = weights/sum(weights)
-    return probs
+            weights.append(dist.pdf([i, j])/.03)  # TODO: added .03 so that prob in center was = .9, almost always floods
+    # weights = weights/sum(weights)
+    return weights
 
 
 def get_probabilities(num_states):
     return gaussian_probs(num_states)  # TODO: try uniform and random as well for final paper
 
 
-def transition(T, prob_dist, a, s, sp):
-    T[s, a, sp] = (1 if sp == a else 0, 0 if sp == a else random.random() < prob_dist[s])  # 3D 100x100x100 matrix
-    return T  # TODO: make sure T correctly updated
-
-
-def lookahead(U, s, a, S, T, R, gamma, prob_dist):  # TODO: bad to add prob_dist here?
-    # NOTE: Marc's other idea for keeping track of all 5 actions: also return actual U values here... ???
-    # TODO: change to s not S? i.e. below code not current code
-    '''
-    u_new = []
-    for s in S:
-        u_new.append(R[s, a] + gamma * sum(transition(T, prob_dist, a, s, sp)*U[sp] for sp in S))
-    return u_new, T
-    '''
-    return R[s, a] + gamma * sum(transition(T, prob_dist, a, s, sp)*U[sp] for sp in S), T
-
-
-def simulate(prob_dist, a, S, R):
+def simulate(prob_dist, S, R):
+    # TODO: pick 4 parks, set prob_dist to 0 for all of them
     for s in range(len(S)):
         if random.random() < prob_dist[s]:
-            s[1] = 1
+            S[s][1] = 1
             R[s] = -10  # If flooded, reward negatively TODO: try diff values here
         else:
-            s[1] = 0
+            S[s][1] = 0
             R[s] = 10  # # If not flooded, reward poistively TODO: try diff values here
     score = sum(R)
     return score, R, S
 
 
-def forward_search(S, d, U, A, T, R, gamma, prob_dist, best_score, best_comb_parks):
-    if d <= 0:
-        return (None, U(s))  # (a, u)  # TODO: figure out how to find U(s) given that we're looking at at all S
-    best = (None, math.inf)  # (a, u)
-    U_prime = forward_search(S, d, U, A, T, R, gamma, prob_dist, best_score, best_comb_parks)[1][1]
-    # OLD: U_prime = forward_search(s, d - 1, U)[1]  # recurse, get second index which is u
-    for a in A:
-        # Simulate Flooding
-        S[a][0] = 1  # Place park there, change second index of tuple to 1
-        prob_dist[a] = 0  # Change prob of flooding to zero (TODO: need to input last component of all S's in simulate below??)
-        score, R, S = simulate(prob_dist, a, S, R)  # TODO: make sure R and S are correctly updated
+# TODO: newer version, w/o bellman (or recursion?) - no need for T function? TA said good but check w mykel that this is fine
+def forward_search(S, depth, A, T, R, gamma, prob_dist, best_scores, best_comb_parks):
+    for d in range(depth):
+        best = [None, -math.inf]  # (a, u)
+        for a in A:
+            # Simulate Flooding
+            S[a][0] = 1  # Place park there, change second index of tuple to 1
+            temp = prob_dist[a]
+            prob_dist[a] = 0  # Change prob of flooding to zero
 
-        # Get next step of value function
-        u, T = lookahead(U_prime, s, a, S, T, R, gamma)  # (a, u)  # TODO: dont input s, just S
+            score, R, S = simulate(prob_dist, S, R)
 
-        # Save if best  # TODO: this will only save 1 action, not 5, right?!?
-        if u > best[1]:  # best[1] = u
-            best = (a, u)
-        if score > best_score:  # NOTE: Write about this in project: experimenting with different metrics; added this cause unsure if ranking by utility is best or if it's better to rank by my more specific score metric; Marc said try both and write about it!
-            best_score = score
-            best = (a, score)
-            best_comb_parks.append(a)
-    best_comb_parks.append(best[0]) # TODO: Check that this correctly adds previously encoded action to current best action in order to return best five actions
-    return best, best_score, best_comb_parks
+            # Save if best
+            if score > best[1]:  # NOTE: Write about this in project: experimenting with different metrics; added this cause unsure if ranking by utility is best or if it's better to rank by my more specific score metric; Marc said try both and write about it!
+                best = [a, score]
+
+            # Restore state so can choose next action
+            S[a][0] = 0  #
+            prob_dist[a] = temp
+        best_comb_parks.append(best[0])
+        best_scores.append(best[1])
+        S[best[0]][0] = 1
+        prob_dist[best[0]] = 0
+        idx = np.where(A == best[0])
+        A = np.delete(A, idx)
+    return best_comb_parks, best_scores, prob_dist, S
 
 
 def fwd_search():
@@ -105,36 +94,54 @@ def fwd_search():
 
     # Set limit of number of parks we can add given current budget
     park_limit = 5
-    d = park_limit
+    depth = park_limit
 
     # Define probability distribution for flooding in each of the grid ares
     prob_dist = get_probabilities(num_states)
 
     # Formulate/Initialize MDP
     S = []
-    A = np.arange(0, num_states)
+    A = np.arange(0, num_states)  # TODO: check that should start at 1, not 0
     for i in range(num_states): # Initialize states as (whether it has a park there, whether it flooded or not) # NOTE: removed position bc encoded as index of s in S, took out prob of flood bc need deterministic states
-        S.append((0, random.random() < prob_dist[i]))
+        S.append([0, random.random() < prob_dist[i]])
     R = np.zeros(num_states)
     T = np.zeros([num_states, num_states, num_states])
     U = np.zeros(num_states)
     gamma = 0.95  # TODO: play around with diff values
 
     # Find optimal placement of parks using forward search
-    best_score = -math.inf
-    best_comb_parks = []
-    # TODO: need to run fwd search in loop like this, or initialize random s and run fwd search once given any initial state?
-
     '''
     # NOTE: Marc's other idea for getting all 5 actions was to run fwd search 5 times, 1st time run w depth = 5, 2nd time run w depth = 4, etc until d is 1
     for d in depth:
         run fwd search for d = 5,4,3,2,1
     '''
 
-    final_policy, best_score, best_comb_parks = forward_search(S, d, U, A, T, R, gamma, prob_dist, best_score, best_comb_parks)[0]
+    best_scores = []
+    best_comb_parks = []
+
+    # TODO: bellman version, reinstate if we want
+    # final_policy, best_score, best_comb_parks = forward_search_bellman(S, d, U, A, T, R, gamma, prob_dist, best_score, best_comb_parks)[0]
+
+    best_comb_parks, best_scores, prob_dist, S = forward_search(S, depth, A, T, R, gamma, prob_dist, best_scores, best_comb_parks)
+    final_policy = []
+    for a in A:
+        if a in best_comb_parks:
+            final_policy.append(1)
+        else:
+            final_policy.append(0)
+
+    # TODO: ASK; calculate final score which way? do both and compare?
+    final_score, R, S = simulate(prob_dist, S, R)
+    # final_score = sum(best_scores)/len(best_scores)  # Get average score
+
+    # Draw heap map
+    R_map = [R[0:10], R[10:20], R[20:30], R[30:40], R[40:50], R[50:60], R[60:70], R[70:80], R[80:90], R[90:100]]
+    ax = sns.heatmap(R_map, linewidth=0.5)
+    plt.title('Fwd Search Policy: Final Score = ' + str(final_score))
+    plt.savefig('fwd_search')
 
     outputfilename = 'fwd_search.policy'
-    write_policy(final_policy, outputfilename)  # TODO: see what policy output looks like, manipulate as needed to be [0 0 1 0 1 ....] of len |S|
+    write_policy(final_policy, final_score, outputfilename)  # TODO: see what policy output looks like, manipulate as needed to be [0 0 1 0 1 ....] of len |S|
 
     print("time elapsed: {:.2f}s".format(time.time() - start_time))
     return
@@ -143,7 +150,74 @@ fwd_search()
 
 
 
+# TODO: NOTE lookahead, transition, fwd_search commented out below bc mykel said we didnt need to use them,
+#  implemented simpler version above
+'''
+def transition(T, prob_dist, a, s, sp):  # TODO: make T update all states
+    T[s, a, sp] = (1 if sp == a else 0, 0 if sp == a else random.random() < prob_dist[s])  # 3D 100x100x100 matrix
+    # TODO: this? T[S, a, sp] = [(1 if sp == a else 0, 0 if sp == a else random.random() < prob_dist[s]) for s in S]
+    return T  # TODO: make sure T correctly updated
+'''
 
+'''
+def lookahead(U, s, a, S, T, R, gamma, prob_dist):  # TODO: bad to add prob_dist here?
+    # NOTE: Marc's other idea for keeping track of all 5 actions: also return actual U values here... ???
+    # TODO: change to s not S? i.e. below code not current code? make U a dict?
+    # u_new = []
+    # for s in S:
+    #     u_new.append(R[s, a] + gamma * sum(transition(T, prob_dist, a, s, sp) * U[sp] for sp in S))
+    # return u_new, T
+    return R[s, a] + gamma * sum(transition(T, prob_dist, a, s, sp) * U[i] for (i, sp) in enumerate(S)), T
+    # return R[s, a] + gamma * sum(transition(T, prob_dist, a, s, sp)*U[sp] for sp in S), T
+'''
+
+'''
+def forward_search_bellman(s, S, d, U, A, T, R, gamma, prob_dist, best_score, best_comb_parks):
+    if d <= 0:
+        return (None, U(s))  # (a, u)  # TODO: figure out how to find U(s) given that we're looking at at all S
+    best = (None, math.inf)  # (a, u)
+    U_prime = forward_search(S, d, U, A, T, R, gamma, prob_dist, best_score, best_comb_parks)[1][1]  # TODO: change this output
+    # OLD: U_prime = forward_search(s, d - 1, U)[1]  # recurse, get second index which is u
+    for a in A:
+        # Simulate Flooding
+        S[a][0] = 1  # Place park there, change second index of tuple to 1
+        temp = prob_dist[a]
+        prob_dist[a] = 0  # Change prob of flooding to zero (TODO: need to input last component of all S's in simulate below??)
+        score, R, S = simulate(prob_dist, a, S, R)  # TODO: make sure R and S are correctly updated
+
+        # Get next step of value function
+        u, T = lookahead(U_prime, s, a, S, T, R, gamma)  # (a, u)  # TODO: dont input s, just S
+
+        # Save if best  # TODO: this will only save 1 action, not 5, right?!?
+        if u > best[1]:  # best[1] = u
+            best = (a, u)
+            # TODO: add (a, u) to a dictionary, at end sort by u and return top a's?
+        if score > best_score:  # NOTE: Write about this in project: experimenting with different metrics; added this cause unsure if ranking by utility is best or if it's better to rank by my more specific score metric; Marc said try both and write about it!
+            best_score = score
+            best = (a, score)
+            # best_comb_parks.append(a)
+
+        # Restore state so can choose next action
+        S[a][0] = 0  #
+        prob_dist[a] = temp
+    best_comb_parks.append(best[0]) # TODO: Check that this correctly adds previously encoded action to current best action in order to return best five actions
+    return best, best_score, best_comb_parks
+'''
+
+
+'''
+    OR (i dont think so...?)
+    S = [[]*num_states]
+    U = [[]*num_states]
+    A = np.arange(1, num_states)
+    for i in range(num_states):
+        for i in range(num_states): # Initialize states as (whether it has a park there, whether it flooded or not) # NOTE: removed position bc encoded as index of s in S, took out prob of flood bc need deterministic states
+            S[i].append((0, random.random() < prob_dist[i]))
+        U.append(np.zeros(num_states))
+
+    R = np.zeros(num_states)
+    T = np.zeros([num_states, num_states, num_states])
+'''
 
 '''
 def recursive_fwd_search(best_score, prob_dist, S):
@@ -169,3 +243,4 @@ def recursive_fwd_search(best_score, prob_dist, S):
     # TODO: Return best tree path (i.e. optimal placement of 5 parks)
     return best_score
 '''
+
